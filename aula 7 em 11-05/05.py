6b8ae7b7e215fadd85656f636fba57089678aaf2
marcosATr <mmarcos.trannin@gmail.com>
2021-05-12 16:51:19 -0300

    ex 5 att

--- aula 7 em 11-05/05.py
+++ aula 7 em 11-05/05.py
@@ -1,9 +1,18 @@
 # Exercício 5 -Faça um programa que tenha uma função chamada ficha(), que receba dois parâmetros: o nome de um jogador e quantos gols ele marcou.O programa deverá ser capaz de mostrar a ficha do jogador, mesmo que algum dado não tenha sido informado corretamente.
 
-def funcFicha(jogador=0, gols=0):
-    nome = jogador
-    gol = gols
-    print(nome)
-    print(gol)
+def funcFicha():
+    jogador = input('digite o nome do jogador: ')
+    gols = input('digite o número de gols realizados: ')
 
-funcFicha('dido')
+    if jogador.isalpha() == False:
+        jogador = "<DESCONHECIDO>"
+    else:
+        jogador = jogador.casefold().title()
+    
+    if gols.isdigit() == False:
+        gols = "QUANTIDADE DESCONHECIDA DE"
+    
+    print(f'O jogador {jogador} fez {gols} GOLS na temporada.')
+
+
+funcFicha()
