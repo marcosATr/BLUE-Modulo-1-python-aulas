eea26d65d947627f305cabb5f8450ed37b8fef27
marcosATr <mmarcos.trannin@gmail.com>
2021-05-13 09:59:28 -0300

    resolvi o teste com o desafio do index

--- aula 8 em 12-05/testes - acompanhando a aula.py
+++ aula 8 em 12-05/testes - acompanhando a aula.py
@@ -1,5 +1,11 @@
-c = (1, 2, 2, 2, 3, 4, 5)
+l = [1, 2, 2, 2, 3, 4, 5, 18, 2]
 
-for i in c:
-    if i == '2':
-        print(i.index())
+n = 0
+for i in l:
+    n += 1
+    # n é o número total de items na lista
+
+index = range(0, n)
+for item in index:
+    if l[item] == 2:
+        print(item)
