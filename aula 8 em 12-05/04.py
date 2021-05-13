bd939b77d283880d168e126dc1f32f4dc2d6c3f0
marcosATr <mmarcos.trannin@gmail.com>
2021-05-13 07:57:16 -0300

    ex 4 feito

--- aula 8 em 12-05/04.py
+++ aula 8 em 12-05/04.py
@@ -1,1 +1,7 @@
 #04 - Escreva um programa onde o usuário digita uma frase e essa frase retorna sem nenhuma vogal.
+
+frase = input('digite sua frase: ')
+d = ['a', 'e', 'i', 'o', 'u']
+for i in d:
+    frase = frase.replace(i, "")
+print(frase)
