08f6dd93e6bea0b6729af40b805e12087c97abcf
marcosATr <mmarcos.trannin@gmail.com>
2021-05-22 17:13:28 -0300

    [FIX] ZeroDivisionError on exercise 1.

--- aula 16 em 24-05/Para Entregar/01.py
+++ aula 16 em 24-05/Para Entregar/01.py
@@ -5,6 +5,7 @@
    # Mostre na tela qual é o maior;
    # Verifique se o resultado da soma é par ou impar e mostre na tela;
    # Se a multiplicação entre eles for maior que 40, divida pelo resultado da divisão inteira e mostre o resultado na tela. Se não, mostre que a multiplicação não foi maior que 40.
+import sys
 n1 = int(input('Digite o 1º valor: '))
 n2 = int(input('Digite o 2º valor: '))
 if n1 > n2:
@@ -23,7 +24,11 @@ else:
     par_impar = 'A soma dos valores é ímpar!'
 print(f'{par_impar}')
 if (n1 * n2) > 40:
-    resultado = (n1 * n2)/(n1 // n2)
+    try:
+        resultado = (n1 * n2)/(n1 // n2)
+    except (ZeroDivisionError):
+        print('Impossível dividir por zero.')
+        sys.exit(1)
     print(f'O resultado do produto pela divisão inteira é: {resultado}')
 else:
     print(f'A multiplicação de {n1} e {n2} não foi maior que 40!')
